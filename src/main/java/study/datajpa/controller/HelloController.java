fbcfb668056e06567450ffa16f97fccbfa71b611
dongkyu <kimdongkyu5902@hanmail.net>
2021-07-28 09:15:59 +0900

    entity 생성 / 테스트

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package study.datajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,45 @@
+package study.datajpa.entity;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member meber1 = new Member("meber1", 10, teamA);
+        Member meber2 = new Member("meber2", 20, teamA);
+        Member meber3 = new Member("meber3", 30, teamB);
+        Member meber4 = new Member("meber4", 40, teamB);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());;
+        }
+
+
+    }
+
+}
