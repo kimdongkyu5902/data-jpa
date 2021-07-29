facc1ac0a001173613e2f5eaa2c60616aeb23bba
dongkyu <kimdongkyu5902@hanmail.net>
2021-07-29 22:07:27 +0900

    기존 jpa repository 로직/테스트
    중복된 코드가 보이는 레포지토리 코드

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package study.datajpa.repository;
 
+import java.util.List;
+import java.util.Optional;
 import study.datajpa.entity.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,6 +15,23 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public Long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 
     public Member find(Long id) {
